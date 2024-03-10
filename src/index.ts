72392a8fe3e17d68692e7b4394aeffa262a59898
prateek-wayne <lordprateekverma@gmail.com>
2024-03-10 12:43:51 +0530

    Add label to pull request

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,27 @@ import * as github from '@actions/github';
 
 const date=new Date();
 core.debug(`Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${date.getMinutes()} `);
-const run=()=>{
+const run=async()=>{
     core.notice(`Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${date.getMinutes()} `);
     const token=core.getInput( "gh-token");
     const label=core.getInput('label');
-    const ocktokit=github.getOctokit(token);
+    const octokit=github.getOctokit(token);
     const context=github.context;
-    
-    console.log(`Inside the main function ${token}`);
-    console.log(JSON.stringify(context));
+    const pullRequest = context.payload.pull_request;
+    try {
+        if(!pullRequest)
+        {
+            throw new Error('this action can only be run on a PR.')
+        }
+        await octokit.rest.issues.addLabels({
+            owner:context.repo.owner,
+            repo:context.repo.repo,
+            issue_number:pullRequest.number,
+            labels:[label],
+        })
+    } catch (error) {
+        core.setFailed((error as Error)?.message ?? "Unknown error")
+    }
 }
 
 run();
