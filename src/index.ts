33367c98398e1132cd7f391746e65b1f6e07a04d
prateek-wayne <lordprateekverma@gmail.com>
2024-03-10 11:25:02 +0530

    update functionality of main code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-const num:number=10;
-for(let i=0;i<num;i++)
-{
-    console.log(i);
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+const date=new Date();
+core.debug(`Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${date.getMinutes()} `);
+const run=()=>{
+    core.notice(`Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${date.getMinutes()} `);
+    const token=core.getInput( "gh-token");
+    const label=core.getInput('label');
+    const ocktokit=github.getOctokit(token);
+    const context=github.context;
+    console.log(context);
 }
+
+run();
