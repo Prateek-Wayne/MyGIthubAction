9229f7171498c77d4fb4c85fb3f39ee735df0c6b
prateek-wayne <lordprateekverma@gmail.com>
2024-03-20 21:43:16 +0530

    Add actions/checkout step to firstactions.yaml

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,15 @@ core.debug(`Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${d
 const run=async()=>{
     const token = core.getInput('gh-token');
     const octokit = github.getOctokit(token);
-    const { owner, repo, number } = github.context.issue;
-    core.debug(`Owner is:${owner} | Repo: ${repo} | number : ${number}`);
+
+    // const { owner, repo, number } = github.context.issue;
+    // core.debug(`Owner is:${owner} | Repo: ${repo} | number : ${number}`);
+    core.debug(`Owner is:${github.context}`);
     try {
-        const issue=await octokit.rest.issues.get({
-            owner,repo,issue_number:number
-        });
-        core.debug(`Main content :${issue}`);
+        // const issue=await octokit.rest.issues.get({
+        //     owner,repo,issue_number:number
+        // });
+        core.debug(`Main content success :`);
     } catch (error) {
         core.setFailed((error as Error)?.message ?? "Unknown error")
     }
