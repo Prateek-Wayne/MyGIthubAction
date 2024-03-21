714344288c86b68471db07d464999a1feb9e2e3f
prateek-wayne <lordprateekverma@gmail.com>
2024-03-21 21:53:20 +0530

    Remove allowlist.json, update workflow file, and delete unused files

--- src/index.ts
+++ src/index.ts
@@ -1,149 +1,29 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-// type FileData = RestEndpointMethodTypes["repos"]["getContent"]["response"]["data"];
-interface FileData {
-  type: "file" | "symlink" | "submodule" | "dir";
-  size: number;
-  name: string;
-  path: string;
-  content: string;
-  sha: string;
-  url: string;
-  git_url: string | null;
-  html_url: string | null;
-  download_url: string | null;
-  _links: any; // You can specify the structure of _links if needed
-}
-
-const date = new Date();
-core.debug(
-  `Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${date.getMinutes()} `
-);
-
-const createJson = (formData: string): Object => {
-  const lines = formData.split("\n");
-
-  const newLines = lines.filter((i) => i != "");
-  let requiredArray = [];
-  for (let i = 0; i < newLines.length; i++) {
-    if (i % 2 !== 0) {
-      requiredArray.push(newLines[i]);
-    }
-  }
-
-  const configObject = {
-    [requiredArray[0]]: {
-      destination_cidrs: [requiredArray[1]],
-      ports: [
+const date=new Date();
+core.debug(`Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${date.getMinutes()} `);
+const run=async()=>{
+    core.notice(`Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${date.getMinutes()} `);
+    const token=core.getInput( "gh-token");
+    const label=core.getInput('label');
+    const octokit=github.getOctokit(token);
+    const context=github.context;
+    const pullRequest = context.payload.pull_request;
+    try {
+        if(!pullRequest)
         {
-          destination_from_port: requiredArray[2],
-          destination_to_port: requiredArray[3],
-        },
-      ],
-      protocols: [parseInt(requiredArray[4])],
-      connection: requiredArray[5],
-    },
-  };
-  //   console.log(`My Form Data :${JSON.stringify(configObject)}`);
-  return configObject;
-};
-
-const run = async () => {
-  core.debug(
-    ` AGAIN Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${date.getMinutes()} `
-  );
-  const token = core.getInput("gh-token");
-  const octokit = github.getOctokit(token);
-
-  const { owner, repo, number } = github.context.issue;
-  core.debug(`Owner is:${owner} | Repo: ${repo} | number : ${number}`);
-  // core.debug(`Owner is:${github.context}`);
-  try {
-    const issue = await octokit.rest.issues.get({
-      owner,
-      repo,
-      issue_number: number,
-    });
-    const myData = createJson(issue.data?.body as string);
-    console.log(`COntent is this:${JSON.stringify(myData)}`);
-
-    // Fetch the content of the existing file
-    const fileDataResponse = await octokit.rest.repos.getContent({
-      owner,
-      repo,
-      path: "allowlist.json", // Path to the existing file
-    });
-
-    const fileData = fileDataResponse.data as FileData;
-    console.log(
-      `THis is the content of file data: ${JSON.stringify(fileData)}`
-    );
-    if (fileData.type)
-      if (fileData.type !== "file" || !fileData?.content) {
-        throw new Error(
-          "The specified path does not point to a file or the file is empty"
-        );
-      }
-
-    // // Decode the content from base64
-    const fileContent = Buffer.from(fileData?.content, "base64").toString();
-    console.log(`This is my Exact Data Present Loclly ${fileContent}`);
-    const existingData = JSON.parse(fileContent);
-    const newData = { ...existingData, ...myData };
-
-    // new content:
-    const newContent = JSON.stringify(newData);
-
-    // Create a new branch
-    const branchName = `append-form-data-${Date.now()}`;
-    const { data: refData } = await octokit.rest.git.getRef({
-      owner,
-      repo,
-      ref: "heads/main", // Replace 'main' with the name of your default branch if it's different
-    });
-    await octokit.rest.git.createRef({
-      owner,
-      repo,
-      ref: `refs/heads/${branchName}`,
-      sha: refData.object.sha,
-    });
-
-    //  Update the file with the new content
-    await octokit.rest.repos.createOrUpdateFileContents({
-      owner,
-      repo,
-      path: "allowlist.json", // Path to the existing file
-      message: "Append form data", // Commit message
-      content: Buffer.from(newContent).toString("base64"), // Encode the new content to base64
-      sha: fileData.sha, // The blob SHA of the file being replaced
-      branch: branchName,
-    });
-
-    // Create a pull request
-    console.log(`Creating Branch Intitiated`);
-    await octokit.rest.pulls.create({
-      owner,
-      repo,
-      title: "Append form data",
-      head: branchName,
-      base: "main", // Replace 'main' with the name of your default branch if it's different
-    });
-    
-    console.log(`Creating Branch Success`);
-    // Update the file with the new content
-    // await octokit.rest.repos.createOrUpdateFileContents({
-    //   owner,
-    //   repo,
-    //   path: 'allowlist.json', // Path to the existing file
-    //   message: 'Append form data', // Commit message
-    //   content: Buffer.from(newContent).toString('base64'), // Encode the new content to base64
-    //   sha: fileData.sha, // The blob SHA of the file being replaced
-    // });
-  } catch (error) {
-    core.setFailed((error as Error)?.message ?? "Unknown error");
-  }
-};
+            throw new Error('this action can only be run on a PR.')
+        }
+        await octokit.rest.issues.addLabels({
+            owner:context.repo.owner,
+            repo:context.repo.repo,
+            issue_number:pullRequest.number,
+            labels:[label],
+        })
+    } catch (error) {
+        core.setFailed((error as Error)?.message ?? "Unknown error")
+    }
+}
 
 run();
