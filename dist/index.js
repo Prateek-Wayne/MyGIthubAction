4a3855dba23ca4cb16577919344aff0877b6202e
Prateek-Wayne <lordprateekverma@gmail.com>
2024-03-21 11:30:19 +0530

    updated build folder

--- dist/index.js
+++ dist/index.js
@@ -34,23 +34,52 @@ const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
 const date = new Date();
 core.debug(`Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${date.getMinutes()} `);
+const createJson = (formData) => {
+    const lines = formData.split("\n");
+    const newLines = lines.filter((i) => i != "");
+    let requiredArray = [];
+    for (let i = 0; i < newLines.length; i++) {
+        if (i % 2 !== 0) {
+            requiredArray.push(newLines[i]);
+        }
+    }
+    const configObject = {
+        [requiredArray[0]]: {
+            destination_cidrs: [requiredArray[1]],
+            ports: [
+                {
+                    destination_from_port: requiredArray[2],
+                    destination_to_port: requiredArray[3],
+                },
+            ],
+            protocols: [parseInt(requiredArray[4])],
+            connection: requiredArray[5],
+        },
+    };
+    //   console.log(`My Form Data :${JSON.stringify(configObject)}`);
+    return configObject;
+};
 const run = async () => {
-    var _a;
+    var _a, _b;
     core.debug(` AGAIN Main Code Started 🚀 at ${date.getDate()}: ${date.getHours()}: ${date.getMinutes()} `);
-    const token = core.getInput('gh-token');
+    const token = core.getInput("gh-token");
     const octokit = github.getOctokit(token);
     const { owner, repo, number } = github.context.issue;
     core.debug(`Owner is:${owner} | Repo: ${repo} | number : ${number}`);
     // core.debug(`Owner is:${github.context}`);
     try {
         const issue = await octokit.rest.issues.get({
-            owner, repo, issue_number: number
+            owner,
+            repo,
+            issue_number: number,
         });
-        core.debug(`Main content success : ${issue}`);
-        console.log(`Main Content is this:${JSON.stringify(issue)}`);
+        // core.debug(`Main content success : ${issue}`);
+        const myData = createJson((_a = issue.data) === null || _a === void 0 ? void 0 : _a.body);
+        // console.log(`Main Content is this:${JSON.stringify(issue)}`);
+        console.log(`COntent is this:${myData}`);
     }
     catch (error) {
-        core.setFailed((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : "Unknown error");
+        core.setFailed((_b = error === null || error === void 0 ? void 0 : error.message) !== null && _b !== void 0 ? _b : "Unknown error");
     }
 };
 run();
